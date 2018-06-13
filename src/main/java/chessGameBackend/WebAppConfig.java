abbcefa24e7b5f9d2f2e2398f6171ebfadcd673b
Roman <aventawonk@gmail.com>
2018-06-13 23:16:52 +0300

    Add Hibernate

--- src/main/java/chessGameBackend/WebAppConfig.java
+++ src/main/java/chessGameBackend/WebAppConfig.java
@@ -1,16 +1,35 @@
 package chessGameBackend;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
+@ComponentScan(basePackages = "chessGameBackend")
 public class WebAppConfig implements WebMvcConfigurer {
 
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/views/", ".jsp");
+    @Bean
+    public SessionFactory getSessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        try {
+            return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new BeanCreationException("");
+        }
     }
+
 
 }

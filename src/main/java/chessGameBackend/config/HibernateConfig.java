e3e7ffe5f77b8fe35d423075de6a1089e2a9f1ac
Roman <aventawonk@gmail.com>
2018-06-14 23:52:12 +0300

    Add Hibernate config with LocalSessionFactoryBean

---
+++ src/main/java/chessGameBackend/config/HibernateConfig.java
@@ -0,0 +1,30 @@
+package chessGameBackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+public class HibernateConfig {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
+        factoryBean.setPackagesToScan("chessGameBackend.model");
+        return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+}

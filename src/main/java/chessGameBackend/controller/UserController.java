273c1c21af7e4d7f6bcc7b9c442412d1c4c12cea
Roman <aventawonk@gmail.com>
2018-06-21 23:39:44 +0300

    Add update and delete methods to user controller

--- src/main/java/chessGameBackend/controller/UserController.java
+++ src/main/java/chessGameBackend/controller/UserController.java
@@ -4,18 +4,41 @@ import chessGameBackend.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     SessionFactory sessionFactory;
 
-    @PostMapping(value = "/addUser")
+    @PostMapping(value = "/add")
     public String addUser(User user) {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.persist(user);
+        session.flush();
+        session.close();
+    }
+
+    @PostMapping(value = "/delete")
+    public String deleteUser(int id) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        User user = session.find(User.class, id);
+        session.remove(user);
+        session.flush();
+        session.close();
+    }
+
+    @PostMapping(value = "/update")
+    public String updateUser(User user) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(user);
         session.flush();
         session.close();
     }

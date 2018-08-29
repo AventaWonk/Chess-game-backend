0204277e3b49e96d486ac7f7a11fa5f7781c5083
Roman <aventawonk@gmail.com>
2018-08-29 23:49:42 +0300

    Update Hibernate configuration

--- src/main/java/chessGameBackend/config/HibernateConfig.java
+++ src/main/java/chessGameBackend/config/HibernateConfig.java
@@ -1,30 +1,52 @@
 package chessGameBackend.config;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@PropertySource("classpath:/db.properties")
 public class HibernateConfig {
 
     @Autowired
-    private ApplicationContext context;
+    private Environment environment;
 
     @Bean
-    public LocalSessionFactoryBean getSessionFactory() {
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-        factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
-        factoryBean.setPackagesToScan("chessGameBackend.model");
-        return factoryBean;
+    public SessionFactory getSessionFactory() {
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(this.getSettings())
+                .build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addPackage("chessGameBackend.model")
+                .buildMetadata();
+
+        SessionFactoryBuilder sessionFactoryBuilder = metadata.getSessionFactoryBuilder();
+        return sessionFactoryBuilder.build();
     }
 
-    @Bean
-    public HibernateTransactionManager getTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(getSessionFactory().getObject());
-        return transactionManager;
+    private Map<String, String> getSettings() {
+        Map<String, String> settings = new HashMap<>();
+        settings.put(AvailableSettings.DRIVER, environment.getProperty("driver"));
+        settings.put(AvailableSettings.DIALECT, environment.getProperty("dialect"));
+        settings.put(AvailableSettings.URL, environment.getProperty("url"));
+        settings.put(AvailableSettings.USER, environment.getProperty("user"));
+        settings.put(AvailableSettings.PASS, environment.getProperty("pass"));
+        settings.put(AvailableSettings.HBM2DDL_AUTO, environment.getProperty("hbm2dll_auto"));
+
+        return settings;
     }
+
 }
